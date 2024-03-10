cab0f6791e86cb7fa8521cf080cb2e8bdacc9bdf
huangzichen <huangzch2022@shanghaitech.edu.cn>
2024-03-10 20:37:49 +0800

    second commit

--- vector.c
+++ vector.c
@@ -2,19 +2,33 @@
 #include <stdlib.h>
 
 Vector *vector_create(void) {
-  Vector *vector = malloc(sizeof(Vector));
-  vector->size = 0;
-  vector->capacity = VECTOR_INITIAL_CAPACITY;
-  vector->data = malloc(sizeof(double) * vector->capacity);
-  return vector;
+    Vector *vector = malloc(sizeof(Vector));
+    if (vector == NULL) {
+        printf("Failed to allocate memory for vector structure\n");
+        return NULL;
+    }
+    vector->size = 0;
+    vector->capacity = VECTOR_INITIAL_CAPACITY;
+    vector->data = malloc(sizeof(double) * vector->capacity);
+    if (vector->data == NULL) {
+        printf("Failed to allocate memory for data\n");
+        free(vector);
+        return NULL;
+    }
+    return vector;
 }
 
 void vector_push(Vector *vector, double element) {
-  if (vector->size == vector->capacity) {
-    vector->capacity *= 2;
-    vector->data = realloc(vector->data, sizeof(double) * vector->capacity);
-  }
-  vector->data[vector->size++] = element;
+    if (vector->size == vector->capacity) {
+        vector->capacity *= 2;
+        double *new_data = realloc(vector->data, sizeof(double) * vector->capacity);
+        if (new_data == NULL) {
+            printf("Failed to reallocate memory for data\n");
+            return;
+        }
+        vector->data = new_data;
+    }
+    vector->data[vector->size++] = element;
 }
 
 double vector_get(const Vector *vector, int index) {
